c8cc96005c7caca1e976d4fe2b7470abce617995
octopenyo <95439331+octopenyo@users.noreply.github.com>
2025-05-31 01:19:24 +0300

    predict.py

--- script/predict.py
+++ script/predict.py
@@ -1,16 +1,1 @@
-from cog import BasePredictor, Input
-from diffusers import StableDiffusionPipeline
-import torch
-
-class Predictor(BasePredictor):
-    def setup(self):
-        self.pipe = StableDiffusionPipeline.from_pretrained(
-            "CompVis/stable-diffusion-v1-4",
-            torch_dtype=torch.float16
-        ).to("cuda")
-
-    def predict(self, prompt: str = Input(description="Text prompt")) -> str:
-        image = self.pipe(prompt).images[0]
-        output_path = "/tmp/output.png"
-        image.save(output_path)
-        return output_path
+delete duplicate predict.py from /script
