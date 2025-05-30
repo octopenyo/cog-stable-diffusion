92e598d5727e8316c78cbc63a588ecb372300b8e
octopenyo <95439331+octopenyo@users.noreply.github.com>
2025-05-30 09:20:08 +0300

    predict.py

--- predict.py
+++ predict.py
@@ -18,7 +18,7 @@ from diffusers.pipelines.stable_diffusion.safety_checker import (
 
 # MODEL_ID refers to a diffusers-compatible model on HuggingFace
 # e.g. prompthero/openjourney-v2, wavymulder/Analog-Diffusion, etc
-MODEL_ID = "stabilityai/stable-diffusion-2-1"
+MODEL_ID = "prompthero/openjourney"
 MODEL_CACHE = "diffusers-cache"
 SAFETY_MODEL_ID = "CompVis/stable-diffusion-safety-checker"
 
@@ -36,6 +36,9 @@ class Predictor(BasePredictor):
             safety_checker=safety_checker,
             cache_dir=MODEL_CACHE,
             local_files_only=True,
+            revision="fp16",
+            torch_dtype=torch.float16,
+            use_safetensors=True,
         ).to("cuda")
 
     @torch.inference_mode()
